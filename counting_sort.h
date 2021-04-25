f343da7eb384875b84bfe613ae8f247dbe5cf3ec
Anton Rybianov <apollo301269@gmail.com>
2021-04-26 00:14:12 +0300

    Fix naming

--- counting_sort.h
+++ counting_sort.h
@@ -4,26 +4,26 @@ class CountingSort {
 public:
     template <class RandomIt, class Unary>
     void Sort(RandomIt begin, RandomIt end, Unary to_unsigned) {
-        size_t max = 0;
+        size_t max_value = 0;
         std::for_each(begin, end, [&](const auto& value) {
-            max = std::max(max, to_unsigned(value));
+            max_value = std::max(max_value, to_unsigned(value));
         });
 
-        counts_.assign(max + 2, 0);
+        counts_.assign(max_value + 2, 0);
 
         std::for_each(begin, end, [&](const auto& value) {
             ++counts_[to_unsigned(value) + 1];
         });
 
-        for (size_t i = 1; i < counts_.size(); ++i) {
-            counts_[i] += counts_[i - 1];
+        for (size_t index = 1; index < counts_.size(); ++index) {
+            counts_[index] += counts_[index - 1];
         }
 
-        indexes_.resize((std::distance(begin, end)));
+        sorted_indexes_.resize((std::distance(begin, end)));
 
-        for (size_t index = 0; index < indexes_.size(); ++index) {
+        for (size_t index = 0; index < sorted_indexes_.size(); ++index) {
             auto counts_index = to_unsigned(begin[index]);
-            indexes_[counts_[counts_index]] = index;
+            sorted_indexes_[counts_[counts_index]] = index;
             ++counts_[counts_index];
         }
 
@@ -33,20 +33,20 @@ public:
 private:
     template <class RandomIt>
     void MakeSorted(RandomIt begin) {
-        inverse_indexes_.resize(indexes_.size());
-        for (size_t i = 0; i < inverse_indexes_.size(); ++i) {
-            inverse_indexes_[indexes_[i]] = i;
+        inverse_indexes_.resize(sorted_indexes_.size());
+        for (size_t index = 0; index < inverse_indexes_.size(); ++index) {
+            inverse_indexes_[sorted_indexes_[index]] = index;
         }
 
-        for (size_t index = 0; index < indexes_.size(); ++index) {
-            std::swap(begin[index], begin[indexes_[index]]);
-            indexes_[inverse_indexes_[index]] = indexes_[index];
-            inverse_indexes_[indexes_[index]] = inverse_indexes_[index];
+        for (size_t index = 0; index < sorted_indexes_.size(); ++index) {
+            std::swap(begin[index], begin[sorted_indexes_[index]]);
+            sorted_indexes_[inverse_indexes_[index]] = sorted_indexes_[index];
+            inverse_indexes_[sorted_indexes_[index]] = inverse_indexes_[index];
         }
     }
 
 private:
     std::vector<size_t> counts_;
-    std::vector<size_t> indexes_;
+    std::vector<size_t> sorted_indexes_;
     std::vector<size_t> inverse_indexes_;
 };
